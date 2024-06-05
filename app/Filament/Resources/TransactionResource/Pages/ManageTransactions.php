5790e63a620ee75aba3bd0534e9db98b644edd6d
PHPdro <pedro110302@gmail.com>
2024-06-05 10:51:53 -0300

    refactor: change transaction resource to simple modal layout

---
+++ app/Filament/Resources/TransactionResource/Pages/ManageTransactions.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\TransactionResource\Pages;
+
+use App\Filament\Resources\TransactionResource;
+use App\Models\Transaction;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageTransactions extends ManageRecords
+{
+    protected static string $resource = TransactionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            TransactionResource\Widgets\TransactionOverview::class,
+        ];
+    }
+}
