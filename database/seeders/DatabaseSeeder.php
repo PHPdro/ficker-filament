40be25f9b601ad2ff138ace043f8ec3ea3b2e8ef
PHPdro <pedro110302@gmail.com>
2024-07-21 14:56:45 -0300

    database: add minor changes to factories and migrations and update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Transaction;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,175 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Transaction::factory(3)->create([
-            'date' => '2023-01-15',
+        User::create([
+            'name' => 'Pedro',
+            'email' => 'pedro110302@gmail.com',
+            'password' => Hash::make('pedro123'),
+            // 'password_confirmation' => Hash::make('pedro123'),
         ]);
-        Transaction::factory(7)->create([
-            'date' => '2023-01-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2023-02-15',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2023-02-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(4)->create([
-            'date' => '2023-03-15',
-        ]);
-        Transaction::factory(6)->create([
-            'date' => '2023-03-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(8)->create([
-            'date' => '2023-04-15',
-        ]);
-        Transaction::factory(2)->create([
-            'date' => '2023-04-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(4)->create([
-            'date' => '2023-05-15',
-        ]);
-        Transaction::factory(6)->create([
-            'date' => '2023-05-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2023-06-15',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2023-06-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(3)->create([
-            'date' => '2023-07-15',
-        ]);
-        Transaction::factory(7)->create([
-            'date' => '2023-07-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2023-08-15',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2023-08-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(4)->create([
-            'date' => '2023-09-15',
-        ]);
-        Transaction::factory(6)->create([
-            'date' => '2023-09-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(7)->create([
-            'date' => '2023-10-15',
-        ]);
-        Transaction::factory(3)->create([
-            'date' => '2023-10-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(4)->create([
-            'date' => '2023-11-15',
-        ]);
-        Transaction::factory(6)->create([
-            'date' => '2023-11-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2023-12-15',
-        ]);
-        Transaction::factory(2)->create([
-            'date' => '2023-12-15',
+
+        Category::factory(5)->create();
+        
+        Transaction::factory(50)->create();
+
+        Transaction::factory(50)->create([
             'type' => 'expense',
         ]);
 
-
-        Transaction::factory(3)->create([
-            'date' => '2024-01-15',
-        ]);
-        Transaction::factory(7)->create([
-            'date' => '2024-01-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2024-02-15',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2024-02-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(4)->create([
-            'date' => '2024-03-15',
-        ]);
-        Transaction::factory(6)->create([
-            'date' => '2024-03-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(8)->create([
-            'date' => '2024-04-15',
-        ]);
-        Transaction::factory(2)->create([
-            'date' => '2024-04-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(4)->create([
-            'date' => '2024-05-15',
-        ]);
-        Transaction::factory(6)->create([
-            'date' => '2024-05-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2024-06-15',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2024-06-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(3)->create([
-            'date' => '2024-07-15',
-        ]);
-        Transaction::factory(7)->create([
-            'date' => '2024-07-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2024-08-15',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2024-08-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(4)->create([
-            'date' => '2024-09-15',
-        ]);
-        Transaction::factory(6)->create([
-            'date' => '2024-09-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(7)->create([
-            'date' => '2024-10-15',
-        ]);
-        Transaction::factory(3)->create([
-            'date' => '2024-10-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(4)->create([
-            'date' => '2024-11-15',
-        ]);
-        Transaction::factory(6)->create([
-            'date' => '2024-11-15',
-            'type' => 'expense',
-        ]);
-        Transaction::factory(5)->create([
-            'date' => '2024-12-15',
-        ]);
-        Transaction::factory(2)->create([
-            'date' => '2024-12-15',
-            'type' => 'expense',
-        ]);
     }
 }
